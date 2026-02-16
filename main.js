a8500b0c7e06599f82234fde7b4d684de2a4c9a4
mariya35ma-cloud <mariya35ma@gmail.com>
2026-02-16 13:28:42 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 const { app, BrowserWindow } = require("electron");
+const path = require("path");
 
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1200,
-    height: 800
+    width: 1400,
+    height: 900,
+    webPreferences: {
+      preload: path.join(__dirname, "preload.js")
+    }
   });
 
-  win.loadURL("https://google.com");
+  win.loadFile("index.html");
 }
 
 app.whenReady().then(createWindow);
